5e080e0dbb8659ae066fec894e9e92803c1b6f81
Viktor Podzigun <viktor.podzigun@gmail.com>
2022-07-16 09:12:33 +0200

    Upgraded Scala.js to v1.5.1 (#3)

--- project/src/main/scala/definitions/MaterialUiCodeGen.scala
+++ project/src/main/scala/definitions/MaterialUiCodeGen.scala
@@ -2,10 +2,11 @@ package definitions
 
 import sbt.Keys._
 import sbt._
-import scoverage.ScoverageKeys.coverageExcludedPackages
 import scalajsbundler.sbtplugin.ScalaJSBundlerPlugin.autoImport._
+import scommons.sbtplugin.project.CommonNodeJsModule
+import scoverage.ScoverageKeys.coverageExcludedPackages
 
-object MaterialUiCodeGen extends NodeJsModule {
+object MaterialUiCodeGen extends MaterialUiModule with CommonNodeJsModule {
 
   override val id = "scommons-material-ui-codegen"
 
@@ -13,7 +14,7 @@ object MaterialUiCodeGen extends NodeJsModule {
 
   override def definition: Project = super.definition
     .settings(
-      skip in publish := true,
+      publish / skip := true,
       publish := ((): Unit),
       publishLocal := ((): Unit),
       publishM2 := ((): Unit),
@@ -22,7 +23,7 @@ object MaterialUiCodeGen extends NodeJsModule {
         "scommons.doctrine.raw" +
           ";scommons.reactdocgen.raw",
 
-      npmDependencies in Compile ++= Seq(
+      Compile / npmDependencies ++= Seq(
         "doctrine" -> "^3.0.0",
         "react-docgen" -> "^5.4.0"
       )
